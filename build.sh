38ccbea8fbb706a7bc1284f07325fe6d75e1b047
Henrique Detal <henrique.silveira1802@usp.br>
2023-11-12 10:16:34 -0300

    ajustando pastas

--- build.sh
+++ build.sh
@@ -5,3 +5,16 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
+# adicione linhas abaixo
+python manage.py migrate
+
+# create superuser if missing
+cat <<EOF | python manage.py shell
+import os
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+User.objects.filter(username=os.environ["DJANGO_SUPERUSER_USERNAME"]).exists() or \
+    User.objects.create_superuser(os.environ["DJANGO_SUPERUSER_USERNAME"], os.environ["DJANGO_SUPERUSER_EMAIL"], os.environ["DJANGO_SUPERUSER_PASSWORD"])
+EOF
