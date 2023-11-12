f1a1f9426120ddb42652bdc586ca541564aa9642
Henrique Detal <henrique.silveira1802@usp.br>
2023-11-12 10:02:09 -0300

    ajustando detalhes finais em url.py

--- BYGsite/destinos/urls.py
+++ BYGsite/destinos/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 app_name = 'destinos'
 urlpatterns = [
-    path('', views.MovieListView.as_view(), name='index'), 
+    path('', views.DestinoListView.as_view(), name='index'), 
     path('search/', views.search_destinos, name='search'),
     path('create/', views.create_destino, name='create'),
-    path('<int:pk>/', views.MovieDetailView.as_view(), name='detail'),
+    path('<int:pk>/', views.DestinoDetailView.as_view(), name='detail'),
 ]
