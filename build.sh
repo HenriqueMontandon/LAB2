49fae8b1a02293e0f169ff5fb1f6823872d43cd1
Henrique Detal <henrique.silveira1802@usp.br>
2023-11-20 22:18:44 -0300

    mudando build.sh

--- build.sh
+++ build.sh
@@ -1,20 +1,29 @@
-#!/usr/bin/env bash
-# exit on error
 set -o errexit
 
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-# adicione linhas abaixo
+
+python manage.py makemigrations
 python manage.py migrate
 
-# create superuser if missing
 cat <<EOF | python manage.py shell
 import os
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group, Permission
 
 User = get_user_model()
 
-User.objects.filter(username=os.environ["DJANGO_SUPERUSER_USERNAME"]).exists() or \
+if not User.objects.filter(username=os.environ["DJANGO_SUPERUSER_USERNAME"]).exists():
     User.objects.create_superuser(os.environ["DJANGO_SUPERUSER_USERNAME"], os.environ["DJANGO_SUPERUSER_EMAIL"], os.environ["DJANGO_SUPERUSER_PASSWORD"])
+
+basic_users_group, _ = Group.objects.get_or_create(name="Basic users")
+
+permissions = ["add_user", "view_user", "add_list", "view_list", "add_comentario", "view_investment"]
+permissions_objs = [Permission.objects.get(codename=c) for c in permissions]
+
+for perm in permissions_objs:
+    basic_users_group.permissions.add(perm)
+
+basic_users_group.save()
 EOF
